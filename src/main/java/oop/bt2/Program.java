2054bd8dcc474dd2a294bf066c8820d98e87e8fe
ADMIN <ADMIN@PC341>
2024-08-20 14:40:45 +0700

    bai tap lab02

---
+++ src/main/java/oop/bt2/Program.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop.bt2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Program {
+    public static void main(String[] args) {
+        Account input1 = new Account ("TK01")
+    }
+}
